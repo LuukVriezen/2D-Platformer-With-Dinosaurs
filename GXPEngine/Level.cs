61ebb71be55cc7ec94460bb26f7fbb8e20cc23b1
LuukVriezen <luukvriezen@gmail.com>
2014-11-19 14:32:57 +0100

    Added dummy Platform and Player classes. Started working on Level.

--- GXPEngine/Level.cs
+++ GXPEngine/Level.cs
@@ -4,11 +4,64 @@ namespace GXPEngine
 {
 	public class Level : GameObject
 	{
+		private int[,] tileData;
+		private int tileSize;
 
-
-		public Level()
+		public Level(/*Temporarily disabled: int tilesX, int tilesY*/)
 		{
+			//Set default values
+			tileSize = 32;
 
+			//Temporarily disabled: tileData = new int[tilesX, tilesY];
+
+			//TEMP
+			//0 = nothing
+			//1 = platform
+			//2 = player
+			tileData = new int[,] {
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0 }
+			};
+			//TEMPEND
+			
+			
+		}
+
+		public void FillLevel()
+		{
+			for (int x = 0; x < tileData.GetLength(0); x++) {
+				for (int y = 0; y < tileData.GetLength(1); y++) {
+					switch(tileData[x, y])
+					{
+						case 1:
+							Platform platform = new Platform();
+							platform.SetXY(x * tileSize, y * tileSize);
+							this.AddChild(platform);
+							break;
+
+						case 2:
+							Player player = new Player();
+							player.SetXY(x * tileSize, y * tileSize);
+							this.AddChild(player);
+							break;
+
+						default:
+							break;
+					}
+				}
+			}
 		}
 	}
 }
