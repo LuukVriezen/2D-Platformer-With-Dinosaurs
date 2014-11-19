3cda0d7755c7a1ba8b1fb24d3c90ba21992364af
KingdomHearts <michael_bossink@hotmail.com>
2014-11-19 11:46:43 +0100

    Projectile and Collactables are added
    Projectile and Collactables are added

---
+++ GXPEngine/GXPEngine/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine.GXPEngine
+{
+    class Collectable
+    {
+        //Fields of the items that are collectable.
+        int totalScore = 0;
+
+        //Rum
+        //
+
+        public Collectable()
+        {
+            //Constructor of collectables
+        }
+
+        public void Collosion(GameObject sprite)
+        {
+            
+        }
+
+        
+
+    }
+}
---
+++ GXPEngine/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class Projectile
+    {
+        //Fields of Projectile
+        float xSpeed = 0.0f;
+        float ySpeed = 0.0f;
+        int lengthOfBeam = 0;
+        int distence = 0;
+        //delay
+        //back by firing
+        //dubstepgun
+        //pistols
+
+        public Projectile()
+        {
+            //Constructor of Projectile. Already made for later use.
+        }
+
+        public void Pistolshot()
+        {
+
+        }
+
+
+
+    }
+}
