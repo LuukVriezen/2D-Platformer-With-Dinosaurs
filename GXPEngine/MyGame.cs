1921a474314ce8fe8130e1e9fac77b4ec6c01b94
LuukVriezen <luukvriezen@gmail.com>
2014-11-19 16:47:58 +0100

    Added Projectile to MyGame

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,44 +1,25 @@
-<<<<<<< HEAD
 using System;
 using GXPEngine;
 using System.Drawing;
 
-public class MyGame : Game
-{	
-	public MyGame () : base(640, 480, false)
-	{
-        Projectile projectile = new Projectile();
-        this.AddChild(projectile);
-	}
-	
-	void Update () {
-		//empty
+namespace GXPEngine
+{
+
+	public class MyGame : Game
+	{	
+		public MyGame () : base(640, 480, false)
+		{
+			this.AddChild(new Level());
+			this.AddChild(new Projectile());
+		}
+		
+		void Update () {
+			//empty
+		}
+
+		static void Main() {
+			new MyGame().Start();
+		}
 	}
 
-	static void Main() {
-		new MyGame().Start();
-	}
 }
-
-=======
-using System;
-using GXPEngine;
-using System.Drawing;
-
-public class MyGame : Game
-{	
-	public MyGame () : base(640, 480, false)
-	{
-		this.AddChild(new Level());
-	}
-	
-	void Update () {
-		//empty
-	}
-
-	static void Main() {
-		new MyGame().Start();
-	}
-}
-
->>>>>>> Stash
