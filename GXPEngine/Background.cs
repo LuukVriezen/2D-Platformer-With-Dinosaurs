69b691d35944a6fbda6fb803279a6f495ca34588
KingdomHearts <michael_bossink@hotmail.com>
2014-11-26 15:38:01 +0100

    commit background error

--- GXPEngine/Background.cs
+++ GXPEngine/Background.cs
@@ -1,54 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GXPEngine
-{
-    class Background : GameObject
-    {
-        Sprite background;
-        AnimSprite trees;
-
-        float frame = 0.0f;
-        int firstFrame = 0;
-        int lastFrame = 15;
-
-        public Background(int width, int height)
-        {
-            Canvas canvas = new Canvas(width, height);
-            background = new Sprite("../../Assets/IMG/background.jpg");
-            background.width = game.width;
-            background.height = height;
-            AddChild(background);
-            
-            trees = new AnimSprite("../../Assets/IMG/Background 2D.png",1,14);
-            trees.width = width * 2;
-            trees.height = game.height / 2;
-            trees.y = game.height - trees.height;
-            AddChild(trees);
-        }
-
-        public void BackgroundAnimation()
-        {
-
-            frame = frame + 0.09f;
-            if (frame >= lastFrame + 1)
-            {
-                frame = firstFrame;
-            }
-            if (frame < firstFrame)
-            {
-                frame = lastFrame;
-            }
-            trees.SetFrame((int)frame);
-
-        }
-    }
-}
-=======
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -96,4 +46,3 @@ namespace GXPEngine
         }
     }
 }
->>>>>>> origin/master
